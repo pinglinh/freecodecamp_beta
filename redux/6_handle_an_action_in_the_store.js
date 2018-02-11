98f32937dcd8f26d3735ea766cb1b299b5341661
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-02-11 19:12:08 +0000

    Create 6_handle_an_action_in_the_store.js

---
+++ redux/6_handle_an_action_in_the_store.js
@@ -0,0 +1,22 @@
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  if (action.type == 'LOGIN') {
+    return state = {
+       login: true      
+    }  
+  }
+  else {
+    return state
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
