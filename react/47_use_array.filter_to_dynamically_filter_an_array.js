10c2f1e801ab81064a5c9833b58463c8572d3a5c
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-02-11 01:03:42 +0000

    Create 47_use_array.filter_to_dynamically_filter_an_array.js

---
+++ react/47_use_array.filter_to_dynamically_filter_an_array.js
@@ -0,0 +1,49 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    }
+  }
+  render() {
+    const usersOnline = (this.state.users).filter(user => {
+      return user.online
+    })
+    const renderOnline = usersOnline.map((onlineUser) => {
+      return <li key={onlineUser.username}>{onlineUser.username}</li>
+    })
+    return (
+       <div>
+         <h1>Current Online Users:</h1>
+         <ul>
+           {renderOnline}
+         </ul>
+       </div>
+    );
+  }
+};
