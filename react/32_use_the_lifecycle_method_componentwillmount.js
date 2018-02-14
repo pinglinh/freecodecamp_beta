125ee53712d1ed026d66ee7c79f93690133afe01
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-02-14 11:50:00 +0000

    Create 32_use_the_lifecycle_method_componentwillmount.js

---
+++ react/32_use_the_lifecycle_method_componentwillmount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  componentWillMount() {
+      console.log("hello");
+  }
+  
+  render() {
+    return <div />
+  }
+};
