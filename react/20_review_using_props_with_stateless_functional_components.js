648d89e585fa968210c8850f21c46e0ae173fab2
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-02-14 11:37:34 +0000

    Create 20_review_using_props_with_stateless_functional_components.js

---
+++ react/20_review_using_props_with_stateless_functional_components.js
@@ -0,0 +1,34 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper name={""}/>
+      </div>
+    );
+  }
+};
+
+class Camper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    )
+  }
+}
+
+Camper.defaultProps = {
+  name: "CamperBot"
+}
+      
+Camper.propTypes = {
+  name: PropTypes.string.isRequired        
+}
